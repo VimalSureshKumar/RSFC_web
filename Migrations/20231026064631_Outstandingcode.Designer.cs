c332f5b646ad2f55d625b0cfcbb0c77e23d11d3f
Vimal Suresh Kumar <ac116329@avcol.school.nz>
2023-10-26 19:49:03 +1300

    Added a migration from the outstanding changes I made into the models.

--- Migrations/20231026064631_Outstandingcode.Designer.cs
+++ Migrations/20231026064631_Outstandingcode.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RSFC_web.Data;
 
@@ -11,9 +12,11 @@ using RSFC_web.Data;
 namespace RSFC_web.Migrations
 {
     [DbContext(typeof(RSFC_webContext))]
-    partial class RSFC_webContextModelSnapshot : ModelSnapshot
+    [Migration("20231026064631_Outstandingcode")]
+    partial class Outstandingcode
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20231026064631_Outstandingcode.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RSFC_web.Migrations
+{
+    /// <inheritdoc />
+    public partial class Outstandingcode : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Player_Phone_Number",
+                table: "Player",
+                type: "nvarchar(17)",
+                maxLength: 17,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Player_Email",
+                table: "Player",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Manager_Phone_Number",
+                table: "Manager",
+                type: "nvarchar(17)",
+                maxLength: 17,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Coach_Phone_Number",
+                table: "Coach",
+                type: "nvarchar(17)",
+                maxLength: 17,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Player_Phone_Number",
+                table: "Player",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(17)",
+                oldMaxLength: 17,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Player_Email",
+                table: "Player",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Manager_Phone_Number",
+                table: "Manager",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(17)",
+                oldMaxLength: 17);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Coach_Phone_Number",
+                table: "Coach",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(17)",
+                oldMaxLength: 17);
+        }
+    }
+}
